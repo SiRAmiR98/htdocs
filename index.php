ad7dda422c49fe6b9a7e6c767c539a025abe1892
SiRAmiR98 <48069677+SiRAmiR98@users.noreply.github.com>
2020-05-17 12:44:01 +0430

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+$a =$_REQUEST['num1'];
+$b =$_REQUEST['num2'];
+$op =$_REQUEST['op'];
+$Res = null;
+    switch (op) {
+        case 'Sum (+)':
+            $Res = $a + $b;
+            echo "<br>";
+            echo ($Res);
+            break;
+        case 'Subtraction (-)':
+            $Res = $a - $b;
+            echo ($Res);
+            echo "<br>";
+            break;
+        case 'Multiply (*)':
+            $Res = $a * $b;
+            echo ($Res);
+            echo "<br>";
+            break;
+        case 'Division (/)':
+            $Res = $a / $b;
+            echo ($Res);
+            echo "<br>";
+            break;
+        default :
+            $Res = "Check your selected operation or Check Input values...!";
+            echo ($Res);
+            echo "<br>";
+            break;
+    }
+?>
+<html>
+<head>
+    <title>Calculator Design by AmiRNH</title>
+</head>
+<body>
+<form action="" method="get" enctype="multipart/form-data">
+    number 1: <input type="number" name="first number" value="<?php $_GET['num1'] ?>"> <br>
+    number 2: <input type="number" name="second number" value="<?php $_GET['num2'] ?>"> <br>
+    <button value="<?php $_GET['op']?>" type="submit" name="Sum (+)">Sum (+)</button>
+    <button value="<?php $_GET['op']?>" type="submit" name="Subtraction (-)">Subtraction (-)</button>
+    <button value="<?php $_GET['op']?>" type="submit" name="Multiply (*)">Multiply (*)</button>
+    <button value="<?php $_GET['op']?>" type="submit" name="Division (/)">Division (/)</button>
+</form>
+<p>
+    Result is : <? echo ($Res); ?>
+</p>
+</body>
+</html>
